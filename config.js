0cacf821a4c3fb4d874fb872d15fb3b7dec70d11
Fabio Baser <f.baser@me.com>
2017-10-13 14:23:37 +0200

    Init

---
+++ config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  expressConfig: {
+    PORT: 5000
+  },
+
+  mysqlConfig: {
+    host: "sae.cuw2bz4mkgfb.eu-central-1.rds.amazonaws.com",
+    user: "root",
+    password: "11100111",
+    database: "clientServerApp",
+    port: 3306
+  }
+};
